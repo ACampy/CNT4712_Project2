8760b8ecb69a0aa4ce162f0b0d78d69cd76843bb
Unknown <acampaneria143@gmail.com>
2018-04-09 14:44:37 -0400

    Messing around
    Mash up of code

--- Canvas.py
+++ Canvas.py
@@ -26,12 +26,28 @@ from tkinter import *
 
 b1 = "up"
 xold, yold = None, None
+color = "blue"
 
+class GUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
 
+        self.label = Label(master, text="Paint")
+        self.label.pack()
+
+        self.buttonRed = Button(master, text="Red", command=changeRed)
+        self.buttonRed.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+        
+    
 
 def main():
     
     root = Tk()
+    my_gui = GUI(root)  #my_gui needed???
     drawing_area = Canvas(root)
     drawing_area.pack()
     drawing_area.bind("<Motion>", motion)
@@ -54,10 +70,15 @@ def motion(event):
     if b1 == "down":
         global xold, yold
         if xold is not None and yold is not None:
-            event.widget.create_line(xold,yold,event.x,event.y,smooth=TRUE, fill="red")
+            event.widget.create_line(xold,yold,event.x,event.y,smooth=TRUE, fill=color)
                           # here's where you draw it. smooth. neat.
         xold = event.x
         yold = event.y
+
+def changeRed():
+        color = "red"
+        print("Color changed to Red")
+
 
 if __name__ == "__main__":
     main()
